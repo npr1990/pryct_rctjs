e8844490f9303c639676ba3766d11a8146be5215
Nahuel Plensa Rodriguez <Nahuelpr1990@gmail.com>
2022-10-27 19:41:56 -0300

    nav bar

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav className="navBar"> 
+          <h1>MicroProblems</h1> 
+          <a href="#">Inicio</a>
+          <a href="#">Carrito</a>
+          <a href="#">Contacto</a>
+          <a href="#">Otras Paginas</a>
+        </nav>
       </header>
-    </div>
+       <div className='body'>
+
+       </div>
+   </div>
   );
 }
 
